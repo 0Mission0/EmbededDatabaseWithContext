1ddda3111ce48e5795621aea3d9a577659441994
0Mission0 <0mission0@gmail.com>
2019-03-29 10:27:23 +0800

    修改一下排版
    jdbc:embedded-database  沒有指定 URL ，很難知道用哪個 URL 去 H2 Console 檢視 DB

--- src/main/java/idv/mission/example/EmbededDatabaseWithContext/Test.java
+++ src/main/java/idv/mission/example/EmbededDatabaseWithContext/Test.java
@@ -1,16 +1,26 @@
 package idv.mission.example.EmbededDatabaseWithContext;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Test {
-	public static void main(String[] args) {
-		ApplicationContext context = new FileSystemXmlApplicationContext("src/applicationContext.xml");
-		UserDao dao = context.getBean(UserDao.class);
-		User user = dao.findByName("mission");
-		System.out.println(user);
-		System.out.println(user.getId().intValue());
-		System.out.println(user.getName());
-		System.out.println(user.getEmail());
-	}
+    public static void main(String[] args) {
+        FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("src/applicationContext.xml");
+
+        try {
+            System.out.println("You have 30 secs to open H2 console and view the db content.");
+            Thread.sleep(30 * 1000);
+        }
+        catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+
+        UserDao dao = context.getBean(UserDao.class);
+        User user = dao.findByName("mission");
+        System.out.println(user);
+        System.out.println(user.getId().intValue());
+        System.out.println(user.getName());
+        System.out.println(user.getEmail());
+        System.out.println("End");
+        context.close();
+    }
 }
